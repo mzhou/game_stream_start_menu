32ce6315af28f858cd5c3b966a9358ba1bc6f4c9
Michael Zhou <mzhou@cse.unsw.edu.au>
2020-12-14 00:51:25 +1100

    everything working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use winapi::shared::minwindef::{DWORD, WORD};
+use winapi::ctypes::c_int;
+use winapi::shared::minwindef::{DWORD, LPARAM, WORD, WPARAM};
 use winapi::um::winuser;
 
-unsafe fn keyboard_input(wVk: WORD, up: bool) -> winuser::INPUT {
+const HOTKEY_VK: WORD = winuser::VK_F8 as WORD;
+const SIMULATE_VK: WORD = winuser::VK_LWIN as WORD;
+
+unsafe fn keyboard_input(vk: WORD, up: bool) -> winuser::INPUT {
     let mut u: winuser::INPUT_u = std::mem::zeroed();
     *u.ki_mut() = winuser::KEYBDINPUT {
-        wVk,
-        wScan: winuser::MapVirtualKeyA(wVk.into(), winuser::MAPVK_VK_TO_VSC) as WORD,
-        dwFlags: winuser::KEYEVENTF_EXTENDEDKEY | ((up as DWORD) * winuser::KEYEVENTF_KEYUP),
+        wVk: vk,
+        wScan: 0,
+        dwFlags: ((up as DWORD) * winuser::KEYEVENTF_KEYUP),
         time: 0,
         dwExtraInfo: winuser::GetMessageExtraInfo() as usize,
     };
@@ -17,34 +21,35 @@ unsafe fn keyboard_input(wVk: WORD, up: bool) -> winuser::INPUT {
 }
 
 fn main() {
+    let hotkey_id: c_int = 1;
     unsafe {
-        winuser::RegisterHotKey(
-            std::ptr::null_mut(),
-            1,
-            winuser::MOD_ALT as u32,
-            winuser::VK_ESCAPE as u32,
-        );
+        winuser::RegisterHotKey(std::ptr::null_mut(), hotkey_id, 0 as u32, HOTKEY_VK.into());
         let mut msg: winuser::MSG = std::mem::zeroed();
         let mut inputs = [
-            keyboard_input(winuser::VK_LWIN as WORD, false),
-            keyboard_input(winuser::VK_LWIN as WORD, true),
+            keyboard_input(SIMULATE_VK, false),
+            keyboard_input(SIMULATE_VK, true),
         ];
         loop {
-            winuser::GetMessageW(
+            if winuser::GetMessageW(
                 &mut msg as winuser::LPMSG,
                 std::ptr::null_mut(),
                 winuser::WM_HOTKEY,
                 winuser::WM_HOTKEY,
-            );
-            println!("got message");
+            ) <= 0
+            {
+                break;
+            }
+            println!("lparam {} wparam {}", msg.lParam, msg.wParam);
+            if msg.hwnd != std::ptr::null_mut()
+                || msg.message != winuser::WM_HOTKEY
+                || msg.lParam != (HOTKEY_VK as LPARAM) << 16
+                || msg.wParam != hotkey_id as WPARAM
+            {
+                continue;
+            }
             winuser::SendInput(
-                1,
+                inputs.len() as u32,
                 &mut inputs[0] as winuser::LPINPUT,
-                std::mem::size_of::<winuser::INPUT>() as i32,
-            );
-            winuser::SendInput(
-                1,
-                &mut inputs[1] as winuser::LPINPUT,
                 std::mem::size_of::<winuser::INPUT>() as i32,
             );
         }
